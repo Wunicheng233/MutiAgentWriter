3022e867f892075337bbaf006950bb921d2abdd5
Wunicheng233 <2220053188@qq.com>
2026-04-23 08:40:16 +0800

    Clarify deployment options in README and create project UI

--- frontend/src/pages/CreateProject.tsx
+++ frontend/src/pages/CreateProject.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 import { Layout } from '../components/Layout'
 import { Card } from '../components/Card'
